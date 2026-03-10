8622101d7e4587cba3b12558803df72d34ed96d5
walterthegood <waltersalmon2000@gmail.com>
2026-03-10 16:06:46 +0000

    Creo la relacion y el seeder

--- Laravel11-Task/app/Models/Task.php
+++ Laravel11-Task/app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
@@ -11,5 +12,13 @@ class Task extends Model
     use HasFactory;
     use SoftDeletes;
 
-    //
+    protected $fillable = [
+        'title',
+        'description',
+    ];
+
+    public function User(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
