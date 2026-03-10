651845f88ea123446257edc01bb4968de289ffb2
walterthegood <waltersalmon2000@gmail.com>
2026-03-10 16:18:17 +0000

    Vista con el usuario logeado admin

--- Laravel11-Task/resources/views/dashboard.blade.php
+++ Laravel11-Task/resources/views/dashboard.blade.php
@@ -4,6 +4,15 @@
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
             </div>
+            <div class="flex flex-col items-start gap-4 rounded-xl border border-neutral-200 bg-white/50 p-4 dark:border-neutral-700 dark:bg-black/50">
+            
+            <h1 style="font-size: 2rem; font-weight: bold;"> Bienvenido al gestor de tareas </h1>
+            
+            {{ Auth::user()->name }}
+            
+            </div>
+            
+            
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
             </div>
